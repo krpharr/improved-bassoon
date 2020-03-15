d4ae47a7168d47dde1a18a7331bade3b7a0d9375
rpharr <rpharr@richmond.edu>
2020-03-15 12:19:24 -0400

    save

---
+++ public/service-worker.js
@@ -0,0 +1,110 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/dist/bundle.js",
+  "/dist/icons/icon_96x96.png",
+  "/dist/icons/icon_128x128.png",
+  "/dist/icons/icon_192x192.png",
+  "/dist/icons/icon_256x256.png",
+  "/dist/icons/icon_384x384.png",
+  "/dist/icons/icon_512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const status = await navigator.permissions.query({
+  name: 'periodic-background-sync',
+});
+if (status.state === 'granted') {
+  // Periodic background sync can be used.
+
+} else {
+  // Periodic background sync cannot be used.
+}
+
+async function updateTransaction() {
+  const transactionCache = await caches.open('transactions');
+  await transactionCache.add('/api/transaction');
+}
+
+self.addEventListener('periodicsync', (event) => {
+  if (event.tag === 'update-transactions') {
+    event.waitUntil(updateTransaction());
+  }
+});
+
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  console.log(event.request.url);
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
